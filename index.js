a115d1b3e0e6ccdaff39a6c5687b46d274f27936
ibrahimcobani <ibrahim@imaconsult.com>
2015-08-31 21:03:36 +0300

    bir iki dosya daha ekledim

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-/**
- * Created by ibrahimcobani on 23/07/15.
- */
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World! geç sen dalganı');
+});
+
+var server = app.listen(3000, function () {
+    var host = 'localhost';
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
